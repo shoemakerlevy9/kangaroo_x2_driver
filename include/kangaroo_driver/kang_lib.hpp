e101b4cbfd660309f57fb7b1fce1b7d54f8cb584
Nick Fragale <nicholas.fragale@gmail.com>
2016-11-16 14:19:06 -0600

    Add power_down_all_command

--- include/kangaroo_driver/kang_lib.hpp
+++ include/kangaroo_driver/kang_lib.hpp
@@ -13,5 +13,6 @@ size_t write_kangaroo_position_command(unsigned char address, char channel, int 
 size_t write_kangaroo_speed_command(unsigned char address, char channel, int speed, unsigned char* buffer);
 size_t write_kangaroo_start_command(unsigned char address, char channel, unsigned char* buffer);
 size_t write_kangaroo_get_command(unsigned char address, char channel, char parameter, unsigned char* buffer);
-size_t write_kangaroo_command(unsigned char address, unsigned char command, const unsigned char* data, unsigned char length, unsigned char* buffer);
+size_t write_kangaroo_start_command(unsigned char address, char channel, unsigned char* buffer);
+size_t power_down_all_command(unsigned char address, unsigned char command, const unsigned char* data, unsigned char length, unsigned char* buffer);
 
